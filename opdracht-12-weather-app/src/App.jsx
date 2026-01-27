009a2320028fd2ddc9c381370b7c3b3ad9e71ff2
9024514 <9024514@student.zadkine.nl>
2026-01-27 11:21:20 +0100

    opdracht 12

--- opdracht-12-weather-app/src/App.jsx
+++ opdracht-12-weather-app/src/App.jsx
@@ -4,7 +4,20 @@ function App() {
 
   return (
     <>
-      <h1>Opdracht 12 - Weather App</h1>
+      <section className='mx-auto w-1/2'>
+        <section className='w-48 '>
+          <div className='flex'>
+            <img src="/img/foto2.png" alt="Workcation" />
+            <h1>Work<span className='text-violet-300'>cation</span></h1>
+          </div>
+         <img className='w-64' src="/img/foto.png" alt="Workcation" />
+          <h2 className='text-2xl font-bold'>you can work form anywhere. <span className='text-violet-300'>take advantage of it</span></h2>
+          <p>workation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when your not on vacation</p>
+
+          <button className='bg-violet-300'>Book your escape</button>
+
+        </section>
+      </section>
     </>
   )
 }
